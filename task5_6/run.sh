4efef24ebc106a1f45fd1442cdada9cd049adc39
Daria Petrova <daria.petrova496@gmail.com>
2025-03-25 15:47:53 +0700

    changed port to 1111

--- task5_6/run.sh
+++ task5_6/run.sh
@@ -12,11 +12,20 @@ docker run -d \
   custom_postgres
 
 docker build -t task5_app .
+
 docker run -d \
   --name api_container \
   --network task5_network \
   --env-file .env \
-  -p 1111:8080 \
+  -p 1111:1111 \
   task5_app
+
+docker build -t nginx -f Dockerfile.nginx .
+
+docker run -d \
+  --name nginx_container \
+  --network task5_network \
+  -p 80:80 \
+  nginx
 
 echo "Containers started"
--- task5_6/stop.sh
+++ task5_6/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop api_container db_container
-docker rm api_container db_container
+docker stop nginx_container api_container db_container
+docker rm nginx_container api_container db_container
 docker rmi task5_app
 
 docker network rm task5_network
